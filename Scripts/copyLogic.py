cb8ec972bf92e8dd2aac73957a08b903b1971d28
Dashwii <dashwiibox@gmail.com>
2021-07-07 09:30:35 -0400

    simplified copied file naming. removed give_new_file_name()

--- Scripts/copyLogic.py
+++ Scripts/copyLogic.py
@@ -24,14 +24,9 @@ def get_file_name(origin_directory):
     return file_name
 
 
-def give_new_name(file_name, date, time_stamp):
-    new_file_name = f"{file_name} {date} {time_stamp}"
-    return new_file_name
-
-
 def copy_to_directory(origin_dir, destination_dir, file_name):
     try:
-        new_file_name = give_new_name(file_name, get_date(), get_timestamp())
+        new_file_name = f"{file_name} {get_date()} {get_timestamp()}"
         folder_dir = rf"{destination_dir}\{new_file_name}"
         shutil.copytree(origin_dir, folder_dir)
         print("\nFile copied")
